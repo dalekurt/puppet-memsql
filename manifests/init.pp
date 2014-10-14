7294bcfbcdc434d104a27d48f4fc7b34fab65d88
Dale-Kurt Murray <dalekurt.murray@gmail.com>
2014-10-14 18:09:32 -0500

    Update params with memsql_user and memsql_group

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,12 +15,12 @@
 #   Default: /opt/memsql-src
 #
 # [*memsql_bin_dir*]
-#   Location to install redis binaries.
+#   Location to install memsql binaries.
 #   Default: /opt/memsql
 #
 # === Examples
 #
-# include redis
+# include memsql
 #
 # class { 'memsql':
 #   version        => '3.1',
@@ -38,11 +38,13 @@
 # Copyright 2014 Dale-Kurt Murray, unless otherwise noted.
 #
 
-class (
+class memsql (
   $version        = $memsql::params::version,
   $license        = $memsql::params::license,
   $memsql_src_dir = $memsql::params::memsql_src_dir,
   $memsql_bin_dir = $memsql::params::memsql_bin_dir,
+  $memsql_user    = $memsql::params::memsql_user,
+  $memsql_group   = $memsql::params::memsql_group
 
 ) inherits memsql::params {
 
@@ -51,25 +53,38 @@ class (
 
   $memsql_pkg_name = "memsqlbin_amd64.tar.gz"
   $memsql_pkg      = "${memsql_src_dir}/${memsql_pkg_name}"
-}
 
-file { $memsql_src_dir:
-  ensure => directory,
 
-}
+  file {
+    owner => $memsql_user,
+    group => $memsql_group
+  }
 
-exec { 'get-memsql-pkg':
-  command => "wget http://download.memsql.com/${license}/${memsql_pkg_name}",
-  cwd     => $memsql_src_dir,
-  path    => "/usr/bin",
-  unless  => "test -f ${memsql_pkg}",
-  require => File[$memsql_src_dir],
-}
+  file { $memsql_src_dir:
+    ensure => directory,
+  }
 
-exec { 'unpack-memsql':
-  command => "tar --strip-components 1 -xzf ${memsql_pkg}",
-  cwd     => $memsql_src_dir,
-  path    => '/bin:/usr/bin',
-  unless  => "test -f ${memsql_src_dir}/Makefile",
-  require => Exec['get-memsql-pkg'],
+  exec { 'get-memsql-pkg':
+    command => "wget http://download.memsql.com/${license}/${memsql_pkg_name}",
+    cwd     => $memsql_src_dir,
+    path    => "/usr/bin",
+    unless  => "test -f ${memsql_pkg}",
+    require => File[$memsql_src_dir],
+  }
+
+  exec { 'unpack-memsql':
+    command => "tar --strip-components 1 -xzf ${memsql_pkg}",
+    cwd     => $memsql_src_dir,
+    path    => '/bin:/usr/bin',
+    unless  => "test -f ${memsql_src_dir}/Makefile",
+    require => Exec['get-memsql-pkg'],
+  }
+
+  file { "memsql-init":
+    ensure  => present,
+    path    => "/etc/init.d/memsql",
+    mode    => '0755',
+    content => template('memsql/memsql.init.erb'),
+    notify  => Service["memsql"],
+  }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -18,4 +18,7 @@ class memsql::params {
   $license = '6f1baebd40ea4b14bea2c8bdf2849552'
   $memsql_src_dir = '/opt/memsql-src'
   $memsql_bin_dir = '/opt/memsql'
+  $memsql_user    = 'memsql'
+  $memsql_group   = 'memsql'
+
 }
