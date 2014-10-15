f981d75366b8458243a5d1fe3146f013532a5e89
Dale-Kurt Murray <dalekurt.murray@gmail.com>
2014-10-14 20:04:40 -0500

    Added task to create memsql_bin_dir

--- manifests/init.pp
+++ manifests/init.pp
@@ -77,9 +77,9 @@ class memsql (
     ensure => directory,
   }
 
-#  file { $memsql_bin_dir:
-#    ensure => directory,
-#  }
+  file { $memsql_bin_dir:
+    ensure => directory,
+  }
 
   exec { 'get-memsql-pkg':
     command => "wget http://download.memsql.com/${license}/${memsql_pkg_name}",
